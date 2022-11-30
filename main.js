0071319bb1459995ad96a1a0978d2f08501167ce
Samuel Lee <slee0258@gmail.com>
2022-11-30 11:09:31 +0000

    Completed meme gallery 2 project.

---
+++ main.js
@@ -0,0 +1,28 @@
+var imageUrls = [
+  'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+  'https://c.tenor.com/uYP_Nkq8VPsAAAAd/coding-hello-world.gif',
+  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
+];
+var $gallery = document.querySelector('main');
+var $imageUrlInput = document.querySelector('input');
+var $addImageButton = document.querySelector('button');
+
+$addImageButton.addEventListener('click', function () {
+  if ($imageUrlInput.value !== '') {
+    imageUrls.push($imageUrlInput.value);
+    $imageUrlInput.value = '';
+    updateGallery();
+  }
+});
+
+function updateGallery() {
+  $gallery.innerHTML = '';
+  for (let i = 0; i < imageUrls.length; i++) {
+    var $imageElement = document.createElement('img');
+    $imageElement.className = 'gallery-image';
+    $imageElement.src = imageUrls[i];
+    $gallery.appendChild($imageElement);
+  }
+}
+
+updateGallery();
